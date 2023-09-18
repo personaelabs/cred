8f010359c0572be5a4639186a8ca718ed6cee539
Daniel Tehrani <contact@dantehrani.com>
2023-09-18 09:56:34 +0900

    feat: basic frontend setup

--- packages/frontend/src/pages/_app.tsx
+++ packages/frontend/src/pages/_app.tsx
@@ -1,6 +1,30 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { WagmiConfig, createConfig, mainnet, configureChains } from 'wagmi';
+import type { AppProps } from 'next/app';
+import '@/styles/globals.css';
+import '@rainbow-me/rainbowkit/styles.css';
+import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
+import { publicProvider } from 'wagmi/providers/public';
+
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Nouns Nymz',
+  projectId: '564add972ca30e293482fd9361543d69',
+  chains,
+});
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient,
+  connectors,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={config}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
