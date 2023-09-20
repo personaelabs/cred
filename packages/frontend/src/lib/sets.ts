89b1eab6011006bc0427d1c9ca6e19a9751d484e
Daniel Tehrani <contact@dantehrani.com>
2023-09-20 14:17:37 +0900

    feat: use sets with dev accounts included in non-prod envs

--- packages/frontend/src/lib/sets.ts
+++ packages/frontend/src/lib/sets.ts
@@ -1,1 +1,10 @@
-export const SETS = ['nouns-forker', 'large-contract-deployer', 'large-nft-trader'];
+const SETS = ['nouns-forker', 'large-contract-deployer', 'large-nft-trader'];
+
+if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
+  // Append the test sets
+  SETS.forEach((set) => {
+    SETS.push(`${set}.dev`);
+  });
+}
+
+export default SETS;
