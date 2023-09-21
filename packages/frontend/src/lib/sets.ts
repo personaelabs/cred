37f670cc99d0a6c292ccf6a4871ec28ceee5396a
lsankar4033 <lsankar4033@gmail.com>
2023-09-20 20:49:48 -0700

    feat: static pages for a given twit handle

--- packages/frontend/src/lib/sets.ts
+++ packages/frontend/src/lib/sets.ts
@@ -7,4 +7,44 @@ if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') {
   });
 }
 
+// NOTE: below maps should be in db in the future
+
+// NOTE: placeholders
+export const handleToSet = {
+  '0xFork': 'nouns-forker',
+  '0xDev': 'large-contract-deployer',
+  '0xNFTrader': 'large-nft-trader',
+};
+
+// NOTE: placeholders
+export const handleToProofHash = {
+  '0xFork': '0x0',
+  '0xDev': '0x0',
+  '0xNFTrader': '0x0',
+};
+
+type SetMetadata = {
+  count: number;
+  duneURL: string;
+  displayName: string;
+};
+
+export const setMetadata: { [key: string]: SetMetadata } = {
+  'nouns-forker': {
+    count: 141,
+    duneURL: 'https://dune.com/queries/3037583',
+    displayName: 'Nouns Forker',
+  },
+  'large-contract-deployer': {
+    count: 5152,
+    duneURL: 'https://dune.com/queries/3028106',
+    displayName: 'Large Contract Deployer',
+  },
+  'large-nft-trader': {
+    count: 8708,
+    duneURL: 'https://dune.com/queries/3036968',
+    displayName: 'Large NFT Trader',
+  },
+};
+
 export default SETS;
