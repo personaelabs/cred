25a2e9994cd5ef44960180b257a11b7957470038
Daniel Tehrani <contact@dantehrani.com>
2023-10-18 13:27:38 -0700

    fix: build error

--- packages/frontend/src/lib/utils.ts
+++ packages/frontend/src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { Hex } from 'viem';
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+// Append the "0x" prefix to the string if it doesn't have it
+export const toPrefixedHex = (str: String): Hex => {
+  return (str.includes('0x') ? str : '0x' + str) as Hex;
+};
