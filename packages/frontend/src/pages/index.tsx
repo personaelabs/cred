c8c5319502b381b20f427ed37c33b2404509866f
lsankar4033 <lsankar4033@gmail.com>
2023-10-18 12:42:14 -0700

    feat: hook up eligible + added logic, remove old code

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -4,7 +4,7 @@ import { useProve } from '@/hooks/useProve';
 import { useSubmitProof } from '@/hooks/useSubmitProof';
 import { useCallback, useState } from 'react';
 import { useGetMerkleProof } from '@/hooks/useGetMerkleProof';
-import SETS, { ROOT_TO_SET } from '@/lib/sets';
+import SETS, { ROOT_TO_SET, SET_METADATA } from '@/lib/sets';
 import { Hex } from 'viem';
 import axios from 'axios';
 import {
@@ -42,7 +42,7 @@ export default function Home() {
   const [username, setUsername] = useState<string>('');
 
   // The set to prove membership
-  // TODO: multi-set proving
+  // TODO: multi-set proving will change what we store here
   const [selectedSet, setSelectedSet] = useState<string | undefined>();
   const [proving, setProving] = useState(false);
 
@@ -96,6 +96,7 @@ export default function Home() {
         console.log(username);
         const data = await getUserProofs(username);
 
+        // TODO: cache added sets?
         const _addedSets = data.map((proof: any) => {
           const publicInput = PublicInput.deserialize(
             Buffer.from(proof.publicInput.replace('0x', ''), 'hex'),
@@ -115,37 +116,38 @@ export default function Home() {
     })();
   }, [username, getUserProofs]);
 
-  // TODO: do multi-prove
+  // TODO: do multi-prove if multiple cred at once
   const handleProveClick = useCallback(async () => {
-    if (selectedSet && address) {
-      // TODO: Add a timestamp to the message being signed?
-      const message = username;
-      const sig = await signMessageAsync({ message });
+    console.log('hi');
+    // if (selectedSet && address) {
+    //   // TODO: Add a timestamp to the message being signed?
+    //   const message = username;
+    //   const sig = await signMessageAsync({ message });
 
-      setProving(true);
-      // Get the merkle proof from the backend
-      const merkleProof = await getMerkleProof(selectedSet, address);
+    //   setProving(true);
+    //   // Get the merkle proof from the backend
+    //   const merkleProof = await getMerkleProof(selectedSet, address);
 
-      let proof: Hex;
-      let publicInput: Hex;
-      // When NEXT_PUBLIC_USE_TEST_PROOF is true, we skip the proving step and use dummy proof.
-      // The backend is aware of this dummy proof and will accept it.
-      // This is useful for testing the UI.
-      if (process.env.NEXT_PUBLIC_USE_TEST_PROOF === 'true') {
-        proof = '0x';
-        publicInput = '0x';
-      } else {
-        // Prove!
-        const result = await prove(sig, username, merkleProof);
-        proof = result.proof;
-        publicInput = result.publicInput;
-      }
+    //   let proof: Hex;
+    //   let publicInput: Hex;
+    //   // When NEXT_PUBLIC_USE_TEST_PROOF is true, we skip the proving step and use dummy proof.
+    //   // The backend is aware of this dummy proof and will accept it.
+    //   // This is useful for testing the UI.
+    //   if (process.env.NEXT_PUBLIC_USE_TEST_PROOF === 'true') {
+    //     proof = '0x';
+    //     publicInput = '0x';
+    //   } else {
+    //     // Prove!
+    //     const result = await prove(sig, username, merkleProof);
+    //     proof = result.proof;
+    //     publicInput = result.publicInput;
+    //   }
 
-      // Submit the proof to the backend
-      const proofHash = await submitProof({ proof, publicInput, message, proofVersion: 'v2' });
-      setProofHash(proofHash);
-      setProving(false);
-    }
+    //   // Submit the proof to the backend
+    //   const proofHash = await submitProof({ proof, publicInput, message, proofVersion: 'v2' });
+    //   setProofHash(proofHash);
+    //   setProving(false);
+    // }
   }, [selectedSet, address, username, signMessageAsync, getMerkleProof, submitProof, prove]);
 
   return (
@@ -182,14 +184,16 @@ export default function Home() {
 
             <div className="flex flex-col space-y-1.5">
               {addedSets.length === 0 ? (
-                <Label>No added creddd for {username}</Label>
+                <Label>
+                  No added creddd
+                  {username.length > 0 ? <span> for {username}</span> : <></>}
+                </Label>
               ) : (
                 <div>
                   <Label>Added creddd</Label>
                   <div className="">
                     {addedSets.map((set) => (
-                      // TODO: display based on long name in set metadata
-                      <Badge key={set}>{set}</Badge>
+                      <Badge key={set}>{SET_METADATA[set].displayName}</Badge>
                     ))}
                   </div>
                 </div>
@@ -199,95 +203,43 @@ export default function Home() {
             <Separator />
 
             <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Eligible creddd</Label>
+              {eligibleSets.length === 0 ? (
+                <Label>No eligible creddd for connected addresses</Label>
+              ) : (
+                <div>
+                  <Label htmlFor="framework">Eligible creddd</Label>
 
-              <div>
-                <div className="flex items-center space-x-2">
-                  <Switch id="large-nft-trader" disabled />
-                  <Badge variant="outline">Large NFT Trader</Badge>
+                  <div>
+                    {eligibleSets.map((set) => (
+                      // TODO: set badge name based on set
+                      <div key={set}>
+                        <div className="flex items-center space-x-2">
+                          <Switch id={set} />
+                          <Badge variant="outline">{SET_METADATA[set].displayName}</Badge>
+                        </div>
+
+                        {/* TODO: message when set doesn't correspond to selected address */}
+                        {/* <p className="text-muted-foreground text-sm">
+                          Use account <code>0x321...321</code>
+                        </p> */}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                <p className="text-muted-foreground text-sm">
-                  Connect account <code>0x321...321</code>
-                </p>
-              </div>
-
-              <div className="flex items-center space-x-2">
-                <Switch id="nouns-forker" />
-                <Badge variant="outline">Nouns Fork 0</Badge>
-              </div>
+              )}
             </div>
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button>Add</Button>
+          {isConnected ? (
+            <Button onClick={handleProveClick}>{proving ? 'Adding' : 'Add'}</Button>
+          ) : (
+            <Button onClick={handleProveClick} disabled>
+              Add
+            </Button>
+          )}
         </CardFooter>
       </Card>
     </main>
-    // // Copied the <main> and the <div> tag under it from https://github.com/personaelabs/noun-nyms/blob/main/packages/frontend/src/pages/index.tsx
-    // <main className="flex min-h-screen w-full justify-center bg-gray-50">
-    //   <div className="flex h-full w-full max-w-3xl flex-col gap-4 px-4 py-3 md:px-0 md:py-6 ">
-    //     <div className="mb-16 flex justify-end">
-    //       <ConnectButton
-    //         chainStatus={'none'}
-    //         accountStatus={'address'}
-    //         showBalance={false}
-    //       ></ConnectButton>
-    //     </div>
-    //     <div className="mb-2 flex justify-center">
-    //       <input
-    //         onChange={(e) => {
-    //           setUsername(e.target.value);
-    //         }}
-    //         value={username}
-    //         className="border-b-2 bg-transparent"
-    //         type="text"
-    //         placeholder="username"
-    //       ></input>
-    //     </div>
-    //     <div className="mb-2 flex justify-center">
-    //       <select
-    //         className="border-2 bg-transparent"
-    //         onChange={(e) => {
-    //           setSelectedSet(e.target.value);
-    //         }}
-    //         value={selectedSet}
-    //         placeholder="Select a set"
-    //       >
-    //         <option value="" disabled selected={!selectedSet}>
-    //           Select a set
-    //         </option>
-    //         {eligibleSets.map((set) => (
-    //           // Render the eligible sets
-    //           <option key={set} value={set}>
-    //             {set} (eligible)
-    //           </option>
-    //         ))}
-    //         {
-    //           // Render the ineligible sets as disabled options
-    //           SETS.filter((set) => !eligibleSets.includes(set)).map((set) => (
-    //             <option key={set} value={set} disabled>
-    //               {set} (ineligible)
-    //             </option>
-    //           ))
-    //         }
-    //       </select>
-    //     </div>
-    //     <div className="mb-2 flex justify-center">
-    //       <MainButton
-    //         message={proving ? 'Proving' : 'Prove'}
-    //         handler={handleProveClick}
-    //         disabled={!isConnected}
-    //         loading={proving}
-    //       ></MainButton>
-    //     </div>
-    //     <div className="flex  justify-center">
-    //       {proofHash && (
-    //         <div>
-    //           <p>Done! Proof hash: {proofHash}</p>
-    //         </div>
-    //       )}
-    //     </div>
-    //   </div>
-    // </main>
   );
 }
