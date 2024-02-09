67081afcc973df1b7f6eebe014d1d23056859227
Daniel Tehrani <contact@dantehrani.com>
2024-02-09 21:44:22 +0900

    configure current shell

--- scripts/setup-neon.sh
+++ scripts/setup-neon.sh
@@ -1,3 +1,4 @@
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
 export PATH="$HOME/.cargo/bin:$PATH" && \
+source "$HOME/.cargo/env" && \
 pnpm -F merkle-tree exec pnpm install
