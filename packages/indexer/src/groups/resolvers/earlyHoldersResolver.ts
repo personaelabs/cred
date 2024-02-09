789e672a9aec9760320ee796f36003b64289ca37
Daniel Tehrani <contact@dantehrani.com>
2024-02-09 20:01:14 +0900

    get early holders in batches to reduce memory usage

--- packages/indexer/src/groups/resolvers/earlyHoldersResolver.ts
+++ packages/indexer/src/groups/resolvers/earlyHoldersResolver.ts
@@ -20,29 +20,43 @@ const getEarlyHolders = async (contractId: number): Promise<Hex[]> => {
   const earlinessThreshold = Math.round(Number(numHolders) * 0.05);
 
   // Get the first 5% of holders
-  const transfers = await prisma.eRC20TransferEvent.findMany({
-    select: {
-      to: true,
-    },
-    where: {
-      contractId,
-    },
-    distinct: ['to'],
-    orderBy: [
-      {
-        blockNumber: 'asc',
-      },
-      {
-        transactionIndex: 'asc',
-      },
-      {
-        logIndex: 'asc',
-      },
-    ],
-    take: earlinessThreshold,
-  });
 
-  return transfers.map(transfer => transfer.to as Hex);
+  const earlyHolders = new Set<Hex>();
+  let page = 0;
+  const chunkSize = 1000;
+  while (earlyHolders.size < earlinessThreshold) {
+    const chunk = await prisma.eRC20TransferEvent.findMany({
+      select: {
+        to: true,
+      },
+      where: {
+        contractId,
+      },
+      distinct: ['to'],
+      orderBy: [
+        {
+          blockNumber: 'asc',
+        },
+        {
+          transactionIndex: 'asc',
+        },
+        {
+          logIndex: 'asc',
+        },
+      ],
+      skip: page * chunkSize,
+    });
+
+    chunk.forEach(transfer => {
+      if (earlyHolders.size < earlinessThreshold) {
+        earlyHolders.add(transfer.to as Hex);
+      }
+    });
+
+    page++;
+  }
+
+  return [...earlyHolders];
 };
 
 /**
