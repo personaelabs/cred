f803a66a12961548f0335a92b58fb2409e6d7579
Daniel Tehrani <contact@dantehrani.com>
2024-02-09 21:32:49 +0900

    fix install command

--- scripts/setup-neon.sh
+++ scripts/setup-neon.sh
@@ -1,2 +1,2 @@
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -yes && \
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y && \
 pnpm -F merkle-tree exec pnpm install
