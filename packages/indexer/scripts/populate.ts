e34d3f22c755e24cb0163ec8c2593291c249ea1d
Daniel Tehrani <contact@dantehrani.com>
2024-02-16 18:13:55 -0800

    feat: add populate script

---
+++ packages/indexer/scripts/populate.ts
@@ -0,0 +1,49 @@
+import { Contract } from '@prisma/client';
+import prisma from '../src/prisma';
+
+const CONTRACTS: Pick<
+  Contract,
+  | 'name'
+  | 'type'
+  | 'address'
+  | 'chain'
+  | 'symbol'
+  | 'deployedBlock'
+  | 'targetGroups'
+>[] = [
+  {
+    address: '0x1151cb3d861920e07a38e03eead12c32178567f6',
+    name: 'Bonk',
+    symbol: 'BONK',
+    deployedBlock: BigInt(16628745),
+    targetGroups: ['earlyHolder', 'whale'],
+    chain: 'Ethereum',
+    type: 'ERC20',
+  },
+  {
+    address: '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984',
+    name: 'Uniswap',
+    symbol: 'UNI',
+    deployedBlock: BigInt(10861674),
+    targetGroups: ['earlyHolder', 'whale'],
+    chain: 'Ethereum',
+    type: 'ERC20',
+  },
+];
+
+const populate = async () => {
+  for (const contract of CONTRACTS) {
+    await prisma.contract.upsert({
+      update: contract,
+      create: contract,
+      where: {
+        address_chain: {
+          address: contract.address,
+          chain: contract.chain,
+        },
+      },
+    });
+  }
+};
+
+populate();
