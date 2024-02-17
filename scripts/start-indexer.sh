b8349f8d408c55dd0e1ba0a5223ce01ea6073074
Daniel Tehrani <contact@dantehrani.com>
2024-02-17 14:12:39 -0800

    fix: run db migrations when the indexer starts

--- scripts/start-indexer.sh
+++ scripts/start-indexer.sh
@@ -1,3 +1,4 @@
+pnpm -F db migrate:prod && \
 pnpm -F indexer exec ts-node ./scripts/populate.ts && \
 pnpm -F indexer exec ts-node ./src/syncTransfers.ts && \
 pnpm -F indexer exec ts-node ./src/indexMerkleTree.ts
