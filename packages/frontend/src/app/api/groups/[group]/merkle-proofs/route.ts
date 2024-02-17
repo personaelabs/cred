e49418861757d0e035cb9cf6775b6934cf2e1777
Daniel Tehrani <contact@dantehrani.com>
2024-02-17 15:14:17 -0800

    fix: set `force-dynamic` for all routes so res won't be cached

--- packages/frontend/src/app/api/groups/[group]/merkle-proofs/route.ts
+++ packages/frontend/src/app/api/groups/[group]/merkle-proofs/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import prisma from '@/lib/prisma';
 import { Prisma } from '@prisma/client';
 import { NextRequest } from 'next/server';
--- packages/frontend/src/app/api/groups/route.ts
+++ packages/frontend/src/app/api/groups/route.ts
@@ -1,4 +1,3 @@
-export const fetchCache = 'force-no-store';
 export const dynamic = 'force-dynamic';
 
 import prisma from '@/lib/prisma';
