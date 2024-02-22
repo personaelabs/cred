badf6f116bfa71c88f91aa3d3f40fd54aca972b1
Daniel Tehrani <contact@dantehrani.com>
2024-02-22 11:19:12 +0900

    fix: run the populate script during the credbot build

--- scripts/start-indexer.sh
+++ scripts/start-indexer.sh
@@ -1,4 +1,3 @@
 pnpm -F db migrate:prod && \
-pnpm -F indexer exec ts-node ./scripts/populate.ts && \
 pnpm -F indexer exec ts-node ./src/syncTransfers.ts && \
 pnpm -F indexer exec ts-node ./src/indexMerkleTree.ts
