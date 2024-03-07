ac2a34827bd152fc280abc877d64491c9bc0a720
lsankar4033 <lsankar4033@gmail.com>
2024-03-06 19:28:08 -0800

    chore: test

--- packages/frontend/src/app/api/frames/[[...routes]]/route.tsx
+++ packages/frontend/src/app/api/frames/[[...routes]]/route.tsx
@@ -92,7 +92,8 @@ app.frame('/', c => {
           fontSize: 60,
         }}
       >
-        creddd genesis - you must have creddd on this farcaster account to mint
+        creddd genesis. <br />
+        you must have creddd on this farcaster account to mint.
       </div>
     ),
     intents: [
