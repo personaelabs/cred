23d3c0ad29142a151c80c83d40cb0ed657371c6a
Daniel Tehrani <contact@dantehrani.com>
2024-03-23 14:52:24 -0700

    feat: add link in header

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div className="h-[110px] w-[100vw] flex flex-row justify-end items-center gap-2 px-[100px]">
-      <Link
-        href="https://personae-labs.notion.site/Creddd-9cdf710a1cf84a388d8a45bf14ecfd20"
-        target="_blank"
-      >
-        About
-      </Link>
+    <div className="h-[110px] w-[100vw] flex flex-row justify-between items-center gap-8 px-[100px]">
+      <div>
+        <Link href="/account">My creddd</Link>
+      </div>
+      <div className="flex flex-row gap-8">
+        <Link href="/explore">Explore</Link>
+        <Link
+          href="https://personae-labs.notion.site/Creddd-9cdf710a1cf84a388d8a45bf14ecfd20"
+          target="_blank"
+        >
+          About
+        </Link>
+      </div>
     </div>
   );
 };
