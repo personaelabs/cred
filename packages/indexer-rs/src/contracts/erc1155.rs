f156bf56e027bec7aa95c1150b193dea97ac5298
lsankar4033 <lsankar4033@gmail.com>
2024-03-24 16:23:15 -0700

    chore: change CTG group name

--- packages/indexer-rs/src/contracts/erc1155.rs
+++ packages/indexer-rs/src/contracts/erc1155.rs
@@ -8,8 +8,8 @@ pub fn erc1155_contracts() -> Vec<Contract> {
     vec![Contract {
         id: 0,
         address: "0xa41273d9ecce19051e109d87431002fb1404d392".to_string(),
-        symbol: "crypto-the-game".to_string(),
-        name: "Crypto: The Game Players".to_string(),
+        symbol: "crypto-the-game-s1".to_string(),
+        name: "Crypto: The Game S1".to_string(),
         chain: Chain::Base,
         deployed_block: 11088633,
         target_groups: vec![GroupType::AllHolders],
