757dacb4d456e267d75d950aad4066fa880b396e
lsankar4033 <lsankar4033@gmail.com>
2024-03-25 18:31:44 -0700

    copy: header copy changes

--- packages/frontend/src/components/Header.tsx
+++ packages/frontend/src/components/Header.tsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <div className="h-[80px] w-[100vw] flex flex-row justify-between items-center gap-8 px-[20px] md:px-[100px]">
       <div>
-        <Link href="/account">My creddd</Link>
+        <Link href="/account">Add creddd</Link>
       </div>
       <div className="flex flex-row gap-8">
-        <Link href="/leaderboard">Leaderboard</Link>
+        <Link href="/leaderboard">Creddd 10</Link>
         <Link href="/explore">Explore</Link>
         <Link
           href="https://personae-labs.notion.site/Creddd-9cdf710a1cf84a388d8a45bf14ecfd20"
