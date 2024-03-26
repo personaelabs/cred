f419ff93918a7b4b17c02ece247e806eaff21ad5
Daniel Tehrani <contact@dantehrani.com>
2024-03-25 17:01:20 -0700

    fix: remove the limit

--- packages/indexer-rs/src/group.rs
+++ packages/indexer-rs/src/group.rs
@@ -43,7 +43,7 @@ pub async fn get_groups(pg_client: &tokio_postgres::Client) -> Vec<Group> {
     // Get all groups from the storage
     let result = pg_client
         .query(
-            r#"SELECT "id", "displayName", "typeId", "contractInputs" FROM "Group" LIMIT 1"#,
+            r#"SELECT "id", "displayName", "typeId", "contractInputs" FROM "Group""#,
             &[],
         )
         .await
