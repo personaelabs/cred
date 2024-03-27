02e5fdee8f8c223173676b482b5f1960c5135a0d
Daniel Tehrani <contact@dantehrani.com>
2024-03-27 10:16:16 -0700

    chore: add creddd 10 link in desktop header

--- packages/frontend/src/components/DesktopHeader.tsx
+++ packages/frontend/src/components/DesktopHeader.tsx
@@ -8,6 +8,7 @@ const DesktopHeader = () => {
         <Link href="/account">Add creddd</Link>
       </div>
       <div className="flex flex-row gap-8">
+        <Link href="/leaderboard">Creddd 10</Link>
         <Link href="/explore">Explore</Link>
         <Link
           href="https://personae-labs.notion.site/Creddd-9cdf710a1cf84a388d8a45bf14ecfd20"
