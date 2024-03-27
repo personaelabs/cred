be8de3613a6b03055e0fcbdadbc51d82a2f80f37
Daniel Tehrani <contact@dantehrani.com>
2024-03-27 10:31:14 -0700

    chore: add about link in mobile header

--- packages/frontend/src/components/MobileHeader.tsx
+++ packages/frontend/src/components/MobileHeader.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 const MobileHeader = () => {
   return (
-    <div className="md:hidden h-[80px] w-[100vw] p-[24px]">
+    <div className="md:hidden flex flex-row justify-between h-[80px] w-[100vw] p-[24px]">
       <Link href="https://personaelabs.xyz" target="_blank">
         <Image
           className="opacity-60"
@@ -13,6 +13,12 @@ const MobileHeader = () => {
           width={20}
           height={20}
         />
+      </Link>
+      <Link
+        href="https://personae-labs.notion.site/Creddd-9cdf710a1cf84a388d8a45bf14ecfd20"
+        target="_blank"
+      >
+        About
       </Link>
     </div>
   );
