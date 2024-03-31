eb98fd9050c64e4945b41c437926158ee3e4c77b
Daniel Tehrani <contact@dantehrani.com>
2024-03-31 11:15:09 -0700

    remove: remove mint instruction modal and related code

--- packages/frontend/src/app/api/frames/[[...routes]]/route.tsx
+++ packages/frontend/src/app/api/frames/[[...routes]]/route.tsx
@@ -2,9 +2,6 @@
 /** @jsxImportSource frog/jsx */
 import { Frog } from 'frog';
 import { handle } from 'frog/vercel';
-import prisma from '@/lib/prisma';
-import { adminMint } from '@/lib/zora/zora';
-import { getCustodyAddress } from '@/lib/neynar';
 
 const TEXT_COLOR = '#FDA174';
 const CONTAINER_STYLE = {
@@ -22,8 +19,6 @@ const CONTAINER_STYLE = {
   borderColor: TEXT_COLOR,
 };
 
-const CREDDD_GENESIS_TOKEN_ID = 1;
-
 const { VERCEL_ENV } = process.env;
 
 const app = new Frog({
@@ -38,50 +33,7 @@ const app = new Frog({
   },
 });
 
-/**
- * Check if the user is eligible to mint the NFT.
- */
-const isEligible = async (fid: number) => {
-  const attestationExists = await prisma.fidAttestation.findFirst({
-    where: {
-      fid,
-    },
-  });
-
-  return !!attestationExists;
-};
-
-/**
- * Check if the user has already minted the NFT.
- */
-const hasMinted = async (tokenId: number, fid: number) => {
-  const mingLog = await prisma.mintLog.findUnique({
-    where: {
-      fid_tokenId: {
-        fid,
-        tokenId,
-      },
-    },
-  });
-
-  return !!mingLog;
-};
-
 app.frame('/', c => {
-  const { buttonValue } = c;
-
-  if (buttonValue === 'check') {
-    return checkFrame(c);
-  }
-
-  if (buttonValue === 'about') {
-    return aboutFrame(c);
-  }
-
-  if (buttonValue === 'mint') {
-    return mintFrame(c);
-  }
-
   return c.res({
     action: '/',
     image: (
@@ -97,157 +49,6 @@ app.frame('/', c => {
     intents: [],
   });
 });
-
-/**
- * Check if the user is eligible to mint the NFT.
- * Renders a "Mint" button if the user is eligible,
- * otherwise renders a "Add creddd" button.
- */
-const checkFrame = async (c: any) => {
-  const { frameData } = c;
-
-  console.log('check fid', frameData?.fid);
-
-  if (!frameData) {
-    throw new Error('No frame data');
-  }
-
-  const alreadyMinted = await hasMinted(CREDDD_GENESIS_TOKEN_ID, frameData.fid);
-
-  if (alreadyMinted) {
-    return c.res({
-      image: (
-        <div
-          style={{
-            ...CONTAINER_STYLE,
-            fontSize: 40,
-          }}
-        >
-          the mint has ended
-        </div>
-      ),
-      intents: [],
-    });
-  }
-
-  const canMint = await isEligible(frameData.fid);
-
-  if (canMint) {
-    // The use has creddd.
-    // We can mint the NFT.
-    return c.res({
-      action: '/',
-      image: (
-        <div
-          style={{
-            ...CONTAINER_STYLE,
-            fontSize: 40,
-          }}
-        >
-          the mint has ended
-        </div>
-      ),
-      intents: [],
-    });
-  } else {
-    return c.res({
-      image: (
-        <div
-          style={{
-            ...CONTAINER_STYLE,
-            fontSize: 40,
-          }}
-        >
-          the mint has ended
-        </div>
-      ),
-      intents: [],
-    });
-  }
-};
-
-/**
- * Mint the NFT to the user's custody address
- * and render a "View on Zora" button.
- */
-const mintFrame = async (c: any) => {
-  const { frameData } = c;
-
-  console.log('mint fid', frameData?.fid);
-
-  if (!frameData) {
-    throw new Error('No frame data');
-  }
-
-  const canMint = await isEligible(frameData.fid);
-
-  if (!canMint) {
-    throw new Error('User is not eligible to mint');
-  }
-
-  // Get the custody address of the user.
-  // The NFT will be minted to this address.
-  const custodyAddress = await getCustodyAddress(frameData.fid);
-
-  // Check if the NFT has already been minted
-  const alreadyMinted = await hasMinted(CREDDD_GENESIS_TOKEN_ID, frameData.fid);
-
-  // Mint the NFT if it has not been minted already.
-  if (!alreadyMinted) {
-    // Mint the NFT to the custody address.
-    await adminMint(custodyAddress);
-
-    // Save the mint log.
-    await prisma.mintLog.create({
-      data: {
-        tokenId: CREDDD_GENESIS_TOKEN_ID,
-        fid: frameData.fid,
-      },
-    });
-  }
-
-  return c.res({
-    image: (
-      <div
-        style={{
-          ...CONTAINER_STYLE,
-          fontSize: 40,
-        }}
-      ></div>
-    ),
-    intents: [],
-  });
-};
-
-/**
- * Render the about page.
- */
-const aboutFrame = async (c: any) => {
-  const { frameData } = c;
-
-  if (!frameData) {
-    throw new Error('No frame data');
-  }
-
-  return c.res({
-    image: (
-      <div
-        style={{
-          ...CONTAINER_STYLE,
-          textAlign: 'center',
-          fontSize: 36,
-        }}
-      >
-        creddd is a protocol for attaching onchain reputation to your online
-        identities.
-        <br />
-        it uses zero-knowledge proofs to prove your reputation while obscuring
-        your full onchain activity.
-      </div>
-    ),
-    intents: [],
-  });
-};
 
 export const GET = handle(app);
 export const POST = handle(app);
