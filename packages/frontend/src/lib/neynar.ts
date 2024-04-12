510e58c2aa6a1c6d70be394186209029ecdd296e
lsankar4033 <lsankar4033@gmail.com>
2024-04-12 08:32:10 -0700

    wip: wip feed score

--- packages/frontend/src/lib/neynar.ts
+++ packages/frontend/src/lib/neynar.ts
@@ -3,6 +3,8 @@ import axios from 'axios';
 import { NeynarUserResponse } from '@/app/types';
 import NodeCache from 'node-cache';
 
+import Pool from 'pg-pool';
+
 const cacheTTL = process.env.NODE_ENV === 'development' ? 9999999999 : 60;
 console.log('cacheTTL', cacheTTL);
 const cache = new NodeCache({ stdTTL: cacheTTL, checkperiod: 70 });
@@ -14,6 +16,28 @@ const neynar = axios.create({
     api_key: process.env.NEYNAR_API_KEY,
   },
 });
+
+// NOTE: not robust to neynar db host/db name changes
+const neynarDbConfig = {
+  user: process.env.NEYNAR_DB_USER,
+  password: process.env.NEYNAR_DB_PWD,
+  host: 'db.neynar.com',
+  port: 5432,
+  database: 'farcaster',
+};
+const neynarDbPool = new Pool(neynarDbConfig);
+
+export const getFollowingFids = async (fid: number) => {
+  const client = await neynarDbPool.connect();
+  try {
+    const res = await client.query(
+      `select distinct target_fid from "links" where fid=${fid} and type='follow'`
+    );
+    return res.rows.map((row: any) => parseInt(row.target_fid));
+  } finally {
+    client.release();
+  }
+};
 
 /**
  * Get user data for a given FID from Neynar
