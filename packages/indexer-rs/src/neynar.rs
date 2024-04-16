797e97c1507154ddb39db202067296fb1e05aff8
Daniel Tehrani <contact@dantehrani.com>
2024-04-15 20:40:06 -0700

    chore: paginate the neynar query so it doesn't fail

--- packages/indexer-rs/src/neynar.rs
+++ packages/indexer-rs/src/neynar.rs
@@ -1,4 +1,6 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, time::Instant};
+
+use log::info;
 
 use crate::utils::format_address;
 
@@ -6,10 +8,16 @@ use crate::utils::format_address;
 pub async fn get_verified_addresses(
     fc_replica_client: &tokio_postgres::Client,
 ) -> Result<HashMap<i64, Vec<String>>, tokio_postgres::Error> {
-    // Get all addresses
-    let verified_addresses = fc_replica_client
-        .query(
-            r#"
+    let mut fid_to_verified_addresses = HashMap::<i64, Vec<String>>::new();
+
+    let mut offset = 0i64;
+    let limit = 20000i64;
+    loop {
+        // Get all addresses
+        let start = Instant::now();
+        let verified_addresses = fc_replica_client
+            .query(
+                r#"
                 SELECT
                     fid,
                     ARRAY_AGG(claim->>'address') as "addresses"
@@ -19,28 +27,39 @@ pub async fn get_verified_addresses(
                     deleted_at IS NULL
                 GROUP BY
                     fid
+                ORDER BY
+                    fid ASC
+                OFFSET $1
+                LIMIT $2
                 "#,
-            &[],
-        )
-        .await?;
+                &[&offset, &limit],
+            )
+            .await?;
 
-    let mut fid_to_verified_addresses = HashMap::<i64, Vec<String>>::new();
+        info!("Query time offset={}: {:?}", offset, start.elapsed());
 
-    for verified_address in verified_addresses {
-        let fid: i64 = verified_address.get("fid");
-        let addresses: Vec<String> = verified_address.get("addresses");
-
-        if fid_to_verified_addresses.get(&fid).is_some() {
-            panic!("Duplicate fid: {}", fid);
+        if verified_addresses.is_empty() {
+            break;
         }
 
-        // Format addresses
-        let addresses = addresses
-            .iter()
-            .map(|address| format_address(address))
-            .collect::<Vec<String>>();
+        for verified_address in verified_addresses {
+            let fid: i64 = verified_address.get("fid");
+            let addresses: Vec<String> = verified_address.get("addresses");
 
-        fid_to_verified_addresses.insert(fid, addresses);
+            if fid_to_verified_addresses.get(&fid).is_some() {
+                panic!("Duplicate fid: {}", fid);
+            }
+
+            // Format addresses
+            let addresses = addresses
+                .iter()
+                .map(|address| format_address(address))
+                .collect::<Vec<String>>();
+
+            fid_to_verified_addresses.insert(fid, addresses);
+        }
+
+        offset += limit;
     }
 
     Ok(fid_to_verified_addresses)
