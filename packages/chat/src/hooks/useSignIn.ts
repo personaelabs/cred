9ae4f3cbf69dcaa8e73499b9c5423ad7416c3a29
Daniel Tehrani <contact@dantehrani.com>
2024-05-13 20:40:18 +0900

    feat: register account on the server side

--- packages/chat/src/hooks/useSignIn.ts
+++ packages/chat/src/hooks/useSignIn.ts
@@ -3,7 +3,6 @@ import { SignedInUser } from '@/types';
 import { StatusAPIResponse } from '@farcaster/auth-kit';
 import { useCallback } from 'react';
 import { authSignedInUser } from '@/lib/auth';
-import useRegisterAccount from './useRegisterAccount';
 
 const signIn = async (
   statusApiResponse: StatusAPIResponse
@@ -36,15 +35,12 @@ const signIn = async (
 const useSignIn = () => {
   const queryClient = useQueryClient();
 
-  const { mutateAsync: registerAccount } = useRegisterAccount();
-
   const _signIn = useCallback(
     async (statusApiResponse: StatusAPIResponse) => {
-      const signedInUser = await signIn(statusApiResponse);
-      await registerAccount(signedInUser);
+      await signIn(statusApiResponse);
       await queryClient.invalidateQueries({ queryKey: ['signed-in-user'] });
     },
-    [queryClient, registerAccount]
+    [queryClient]
   );
 
   return { signIn: _signIn };
