a36263a104e9bd700cc45a9e5181232a1cd43ce0
Daniel Tehrani <contact@dantehrani.com>
2024-05-14 20:14:35 +0900

    chore: set ddtags

--- packages/notification/src/lib/logger.ts
+++ packages/notification/src/lib/logger.ts
@@ -5,12 +5,14 @@ const winstonTransports: any[] = [new winston.transports.Console()];
 const { DATADOG_API_KEY } = process.env;
 
 if (DATADOG_API_KEY) {
-  const { RENDER_EXTERNAL_HOSTNAME } = process.env;
+  const { RENDER_EXTERNAL_HOSTNAME, RENDER_GIT_BRANCH } = process.env;
+  console.log('RENDER_EXTERNAL_HOSTNAME', RENDER_EXTERNAL_HOSTNAME);
+  console.log('RENDER_GIT_BRANCH', RENDER_GIT_BRANCH);
 
   winstonTransports.push(
     new winston.transports.Http({
       host: 'http-intake.logs.datadoghq.com',
-      path: `/api/v2/logs?dd-api-key=${DATADOG_API_KEY}&ddsource=nodejs&service=cred-frontend&hostname=${RENDER_EXTERNAL_HOSTNAME || 'localhost'}`,
+      path: `/api/v2/logs?dd-api-key=${DATADOG_API_KEY}&ddsource=nodejs&hostname=${RENDER_EXTERNAL_HOSTNAME || 'localhost'}&ddtags=env:${RENDER_GIT_BRANCH || 'local'}`,
       ssl: true,
     })
   );
