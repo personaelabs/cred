7c49ba8d3b60097ea15f12e91d9f2d3ded3426eb
Daniel Tehrani <contact@dantehrani.com>
2024-05-24 20:02:10 +0900

    chore: uncomment

--- packages/indexer-rs/src/server.rs
+++ packages/indexer-rs/src/server.rs
@@ -17,7 +17,10 @@ pub struct GroupData {
 
 pub type GetGroupsReturnType = Vec<GroupData>;
 
-pub async fn get_groups(group_ids: &Vec<String>, pg_client: &tokio_postgres::Client) -> Vec<GroupData> {
+pub async fn get_groups(
+    group_ids: &Vec<String>,
+    pg_client: &tokio_postgres::Client,
+) -> Vec<GroupData> {
     let rows = pg_client
         .query(
             r#"SELECT "id", "displayName", "typeId" FROM "Group" where "state" = 'Recordable' AND "id" = ANY($1)"#,
@@ -43,13 +46,12 @@ pub async fn get_groups(group_ids: &Vec<String>, pg_client: &tokio_postgres::Cli
 
 pub async fn start_server() {
     let db_options = Options::default();
-//    let rocksdb_conn = Arc::new(DB::open_for_read_only(&db_options, ROCKSDB_PATH, true).unwrap());
+    let rocksdb_conn = Arc::new(DB::open_for_read_only(&db_options, ROCKSDB_PATH, true).unwrap());
 
     let pg_client = init_postgres().await;
 
     let mut io = IoHandler::default();
 
-    /*
     io.add_method("getAddressGroups", move |params: Params| {
         let rocksdb_conn = rocksdb_conn.clone();
         let pg_client = pg_client.clone();
@@ -88,7 +90,6 @@ pub async fn start_server() {
             Ok(json!(group_ids))
         }
     });
-     */
 
     let port = std::env::var("PORT").unwrap_or_else(|_| "3030".to_string());
 
