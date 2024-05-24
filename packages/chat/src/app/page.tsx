f5d087b02df0e024a9b92425fd4df6998d3c9289
Daniel Tehrani <contact@dantehrani.com>
2024-05-24 16:53:47 +0900

    feat: show members in rooms list

--- packages/chat/src/app/page.tsx
+++ packages/chat/src/app/page.tsx
@@ -18,16 +18,53 @@ import { formatEther } from 'viem';
 import usePurchasedRooms from '@/hooks/usePurchasedRooms';
 import ProcessingTxModal from '@/components/ProcessingTxModal';
 import ConnectAddressesSheet from '@/components/ConnectAddressesSheet';
+import { Room } from '@cred/shared';
+import useUsers from '@/hooks/useUsers';
+import AvatarWithFallback from '@/components/Avatar';
+
+interface RoomMembersListProps {
+  room: Room;
+}
+
+const NUM_USERS_TO_SHOW = 5;
+const RoomMembersList = (props: RoomMembersListProps) => {
+  const { joinedUserIds } = props.room;
+
+  const { data: users } = useUsers(joinedUserIds.slice(0, NUM_USERS_TO_SHOW));
+
+  return (
+    <Link href={`/rooms/${props.room.id}/roomInfo`} className="no-underline">
+      <div className="flex flex-row items-center">
+        {users?.map((user, i) => (
+          <div key={i} className={i !== 0 ? 'ml-[-6px]' : ''}>
+            <AvatarWithFallback
+              imageUrl={user?.pfpUrl || null}
+              alt={user?.displayName || 'User'}
+              size={16}
+              name={user?.displayName || ''}
+            ></AvatarWithFallback>
+          </div>
+        ))}
+        {joinedUserIds.length > NUM_USERS_TO_SHOW ? (
+          <div className="text-sm opacity-80">
+            +{joinedUserIds.length - NUM_USERS_TO_SHOW}
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </Link>
+  );
+};
 
 type RoomItemProps = {
-  id: string;
-  name: string;
-  imageUrl: string | null;
+  room: Room;
   canJoin: boolean;
 };
 
 const RoomItem = (props: RoomItemProps) => {
-  const { name, canJoin, id } = props;
+  const { canJoin } = props;
+  const { name, id } = props.room;
   const { mutateAsync: joinRoom, isPending: isJoining, error } = useJoinRoom();
   const router = useRouter();
 
@@ -51,29 +88,39 @@ const RoomItem = (props: RoomItemProps) => {
   }, [error]);
 
   return (
-    <div className="flex mt-4 flex-row items-center justify-between px-5">
-      <div
-        className={`text-md text-wrap w-[55%] ${canJoin ? 'font-bold' : 'font-normal'} ${canJoin ? 'text-primary' : ''}`}
-      >
-        {name}
-      </div>
-      <div className="text-center w-[45%]">
-        {canJoin ? (
-          <Button onClick={onJoinClick} disabled={isJoining} variant="link">
-            Join
-          </Button>
-        ) : (
-          <Button
-            onClick={onPurchaseClick}
-            disabled={isPending}
-            variant="link"
-            className="bg-clip-text text-transparent bg-gradient-to-l from-primary to-[#fdb38f]"
+    <div className="border-b-2 py-2">
+      <div className="flex flex-col px-5">
+        <div className="flex flex-row items-center justify-between">
+          <div
+            className={`text-md text-wrap w-[55%] ${canJoin ? 'font-bold' : 'font-normal'} ${canJoin ? 'text-primary' : ''}`}
           >
-            {keyPrice ? formatEther(keyPrice) : ''}ETH
-          </Button>
-        )}
+            {name}
+          </div>
+          <div className="text-center w-[45%]">
+            {canJoin ? (
+              <Button onClick={onJoinClick} disabled={isJoining} variant="link">
+                Join
+              </Button>
+            ) : (
+              <Button
+                onClick={onPurchaseClick}
+                disabled={isPending}
+                variant="link"
+                className="bg-clip-text text-transparent bg-gradient-to-l from-primary to-[#fdb38f]"
+              >
+                {keyPrice ? formatEther(keyPrice) : ''}ETH
+              </Button>
+            )}
+          </div>
+        </div>
+        <div>
+          <RoomMembersList room={props.room}></RoomMembersList>
+        </div>
+        {
+          // TODO: Move this to a parent component
+          <ProcessingTxModal isOpen={isProcessingTx} />
+        }
       </div>
-      <ProcessingTxModal isOpen={isProcessingTx} />
     </div>
   );
 };
@@ -111,7 +158,11 @@ export default function Home() {
           <InfiniteScroll
             loader={<></>}
             endMessage={<></>}
-            dataLength={purchasableRooms.length}
+            dataLength={
+              purchasableRooms.length +
+              writableRooms.length +
+              purchasedRooms.length
+            }
             hasMore={false}
             next={() => {}}
             scrollThreshold={0.5}
@@ -120,7 +171,7 @@ export default function Home() {
             <Alert>
               <AlertTitle className="flex flx-row justify-between items-center">
                 <div className="opacity-70">
-                  Connect addresses to join more rooms
+                  Connect addresses to find more eligible rooms
                 </div>
                 <Link href="/settings/connected-addresses">
                   <Button variant="secondary">Connect</Button>
@@ -128,27 +179,17 @@ export default function Home() {
               </AlertTitle>
             </Alert>
             {writableRooms.length > 0 || purchasedRooms.length > 0 ? (
-              <div className="px-5 text-center opacity-60">Eligible rooms</div>
+              <div className="px-5 text-center opacity-60 mt-4">
+                Eligible rooms
+              </div>
             ) : (
               <></>
             )}
             {writableRooms.map(room => (
-              <RoomItem
-                id={room.id}
-                key={room.id}
-                name={room.name}
-                imageUrl={room.imageUrl}
-                canJoin={true}
-              ></RoomItem>
+              <RoomItem room={room} key={room.id} canJoin={true}></RoomItem>
             ))}
             {purchasedRooms.map(room => (
-              <RoomItem
-                id={room.id}
-                key={room.id}
-                name={room.name}
-                imageUrl={room.imageUrl}
-                canJoin={true}
-              ></RoomItem>
+              <RoomItem room={room} key={room.id} canJoin={true}></RoomItem>
             ))}
             {purchasableRooms.length > 0 ? (
               <div className="mt-[32px] px-5 text-center opacity-60">
@@ -158,13 +199,7 @@ export default function Home() {
               <></>
             )}
             {purchasableRooms.map(room => (
-              <RoomItem
-                id={room.id}
-                key={room.id}
-                name={room.name}
-                imageUrl={room.imageUrl}
-                canJoin={false}
-              ></RoomItem>
+              <RoomItem room={room} key={room.id} canJoin={false}></RoomItem>
             ))}
           </InfiniteScroll>
         </div>
