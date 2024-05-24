cc847f3c05b65a8bb575e1979c05b24751680507
Daniel Tehrani <contact@dantehrani.com>
2024-05-24 18:08:34 +0900

    chore: point to the remote indexer

--- packages/chat/src/lib/credddRpc.ts
+++ packages/chat/src/lib/credddRpc.ts
@@ -1,6 +1,6 @@
 import { createRpcClient } from '@cred/shared';
 
-// const rpcClient = createRpcClient('https://cred-indexer-41hd.onrender.com');
-const rpcClient = createRpcClient('http://0.0.0.0:3030');
+const rpcClient = createRpcClient('https://cred-indexer-41hd.onrender.com');
+// const rpcClient = createRpcClient('http://0.0.0.0:3030');
 
 export default rpcClient;
