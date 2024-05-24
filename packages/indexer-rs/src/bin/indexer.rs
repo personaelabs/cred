62d82930f6255e4a25b0397ec2ef6e5d2d6dc4fc
Daniel Tehrani <contact@dantehrani.com>
2024-05-24 18:07:08 +0900

    chore: pause indexing

--- packages/indexer-rs/src/bin/indexer.rs
+++ packages/indexer-rs/src/bin/indexer.rs
@@ -35,6 +35,7 @@ async fn main() {
 
     let pg_client = init_postgres().await;
 
+    /* 
     // Open the RocksDB connection
     let mut rocksdb_options = Options::default();
     rocksdb_options.create_if_missing(true);
@@ -160,9 +161,9 @@ async fn main() {
     join!(
         join_all(sync_jobs),
         join_all(indexing_jobs),
-        intrinsic_creddd_sync_job,
-        start_server()
+        intrinsic_creddd_sync_job
     );
+    */
 
     start_server().await;
 }
