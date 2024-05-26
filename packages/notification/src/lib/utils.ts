6fa49ea7234e2422a382a83d4b549a10e1016f60
Daniel Tehrani <contact@dantehrani.com>
2024-05-26 12:35:01 +0900

    fix: nit

--- packages/notification/src/lib/utils.ts
+++ packages/notification/src/lib/utils.ts
@@ -3,8 +3,9 @@ import { app } from '@cred/firebase';
 // @ts-ignore
 const projectId = app.options.credential.projectId;
 
-console.log('projectId', projectId);
-
 export const IS_PROD =
-  process.env.RENDER === 'true' &&
-  (projectId === 'cred-279bb' || projectId === 'staging-423405');
+  (process.env.RENDER === 'true' && process.env.IS_PULL_REQUEST !== 'true') ||
+  projectId === 'staging-423405';
+
+console.log('projectId', projectId);
+console.log('IS_PROD', IS_PROD);
