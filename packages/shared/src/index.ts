0dd27586302e1642f0ad2cf6e998906475362833
Daniel Tehrani <contact@dantehrani.com>
2024-06-04 17:58:14 +0900

    chore: set mainnet address

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -14,7 +14,7 @@ export { default as PortalAbi } from './abi/Portal';
 export { initLogger } from './logger';
 
 export const PORTAL_CONTRACT_ADDRESS =
-  '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+  '0x4c62e19A56dd3F31350cEDB605D234fef7D12d18';
 
 export const PORTAL_SEPOLIA_CONTRACT_ADDRESS =
   '0xb8B0c71AA4e96F002BCd4dE6582B61c80e373E24';
