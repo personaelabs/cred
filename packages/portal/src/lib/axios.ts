37e2343bd5a1d11cb37cf7961ef2ba25b5d8641c
Daniel Tehrani <contact@dantehrani.com>
2024-06-04 15:36:02 +0900

    chore: chat -> portal

--- packages/portal/src/lib/axios.ts
+++ packages/portal/src/lib/axios.ts
@@ -1,8 +1,8 @@
 import axiosBase from 'axios';
 
 const axios = axiosBase.create({
-//  baseURL: 'http://localhost:3000/' 'http://192.168.1.34:3000',
-  baseURL: '/'
+  //  baseURL: 'http://localhost:3000/' 'http://192.168.1.34:3000',
+  baseURL: '/',
 });
 
 export default axios;
