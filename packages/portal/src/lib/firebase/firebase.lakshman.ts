a864fb57228d33286ecb50b0d2d3f498a02ae467
Daniel Tehrani <contact@dantehrani.com>
2024-06-08 11:54:28 +0900

    feat: add lakshman's dev env

---
+++ packages/portal/src/lib/firebase/firebase.lakshman.ts
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: 'AIzaSyDQ0oYEF6wVrS5kHFCxvh7Uy04siQsD0sc',
+  authDomain: 'dev-2-425802.firebaseapp.com',
+  projectId: 'dev-2-425802',
+  storageBucket: 'dev-2-425802.appspot.com',
+  messagingSenderId: '963230250902',
+  appId: '1:963230250902:web:9549733b5a67420d390fb7',
+};
+
+export default config;
