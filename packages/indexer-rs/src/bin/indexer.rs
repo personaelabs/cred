2794bc6a228e12e6a68a84ad6c9eac58a341bee1
Daniel Tehrani <contact@dantehrani.com>
2024-07-12 18:37:19 +0900

    chore: disable intrinsic sync for now

--- packages/indexer-rs/src/bin/indexer.rs
+++ packages/indexer-rs/src/bin/indexer.rs
@@ -133,7 +133,7 @@ async fn main() {
                 GroupType::Static => panic!("Static group type is deprecated"),
                 GroupType::Believer => {
                     Box::new(BelieverIndexer::new(group.clone(), resources.clone()))
-                },
+                }
                 GroupType::BaseSalon => {
                     Box::new(SalonIndexer::new(group.clone(), resources.clone()))
                 }
@@ -146,13 +146,10 @@ async fn main() {
                 GroupType::ArbSalon => {
                     Box::new(SalonIndexer::new(group.clone(), resources.clone()))
                 }
-                GroupType::OpSalon => {
-                    Box::new(SalonIndexer::new(group.clone(), resources.clone()))
-                }
+                GroupType::OpSalon => Box::new(SalonIndexer::new(group.clone(), resources.clone())),
                 GroupType::FriendBagHolder => {
                     Box::new(SalonIndexer::new(group.clone(), resources.clone()))
                 }
-               
             };
 
             // Initialize the tree sync engine for the group
@@ -177,7 +174,7 @@ async fn main() {
     let intrinsic_creddd_sync_engine =
         IntrinsicCredddSyncEngine::new(fc_replica_db, pg_client.clone());
 
-    let intrinsic_creddd_sync_job = intrinsic_creddd_sync_engine.sync();
+        let intrinsic_creddd_sync_job = intrinsic_creddd_sync_engine.sync();
 
     let _pg_client = pg_client.clone();
     let sever_thread = tokio::spawn(async move {
