01e890032be1ba60600db590a9eb652ddb2bebdb
Daniel Tehrani <contact@dantehrani.com>
2024-07-17 15:46:24 +0900

    fix: nit

--- packages/portal/src/hooks/useIsUsernameSet.ts
+++ packages/portal/src/hooks/useIsUsernameSet.ts
@@ -5,7 +5,7 @@ const useIsUsernameSet = () => {
   const { data: signedInUser } = useSignedInUser();
   const { data: user } = useUser(signedInUser?.id || null);
 
-  return user ? user.username !== '' : null;
+  return user ? !!user.username : false;
 };
 
 export default useIsUsernameSet;
